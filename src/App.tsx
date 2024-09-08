1c615a2d85c214af1cf23197aa069e83116321a4
Viiniit Aroaa <viinitaroraa@gmail.com>
2024-09-08 08:46:04 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./assets/sass/main.scss";
 import ProductList from './components/ProductList';
 import ProductDetail from './components/ProductDetail';
 import Cart from './components/Cart';
-import Header from './components/Header';
+import Header from './components/header/index';
 import Footer from './components/Footer';
 const App: React.FC = () => {
   return (
